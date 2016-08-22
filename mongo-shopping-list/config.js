9b3f21f012ee5a6fc415707c05b7291dd836896a
Adam Grushcow <adamgrushcow@gmail.com>
2016-08-22 16:42:12 +0000

    Travis CI testing

--- mongo-shopping-list/config.js
+++ mongo-shopping-list/config.js
@@ -1,7 +1,8 @@
-exports.DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                       (process.env.NODE_ENV === 'production' ?
+//exports.DATABASE_URL = process.env.DATABASE_URL || 
+exports.DATABASE_URL = 'mongodb:mongodb://agrushcow:Tottigoal4527@ds035603.mlab.com:35603/mongo-shopping-list';
+                       //global.DATABASE_URL ||
+                       //(process.env.NODE_ENV === 'production' ?
                             //'mongodb:mongodb://agrushcow:Tottigoal4527@ds035603.mlab.com:35603/mongo-shopping-list'
-                            'mongodb://localhost/shopping-list' :
-                            'mongodb://localhost/shopping-list-dev');
+                            //'mongodb://localhost/shopping-list' :
+                            //'mongodb://localhost/shopping-list-dev');
 exports.PORT = process.env.PORT || 8080;
